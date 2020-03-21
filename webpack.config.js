75eac8e346f18ddd0804eb17261c867c37d99245
Eric Delord <edelord@gmail.com>
2020-03-21 11:19:21 +0100

    generates one umd script and one var script (for scriptable)

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,30 @@
 const path = require('path');
 const config = require('./package.json');
 
-module.exports = {
-  "mode": 'production',
-  "entry": [
-      './src/index.js'
-  ],
-  "output": {
-    "path": path.resolve(__dirname, 'dist'),
-    "filename": 'lidojs-' + config.version + '.js',
-    "library": 'editolido',
-    "libraryTarget": 'umd'
-  },
-};
+// eslint-disable-next-line require-jsdoc
+function createConfig(options) {
+  const targetExtension = (options.target === "umd") ? "" : "." + options.target;
+  const minExtension = (options.minimize === true) ? ".min" : "";
+  return {
+    "mode": "production",
+    "optimization": {
+      "minimize": options.minimize
+    },
+    "entry": [
+      "./src/index.js"
+    ],
+    "output": {
+      "path": path.resolve(__dirname, 'dist'),
+      "filename": 'lidojs-' + config.version + targetExtension + minExtension + '.js',
+      "library": 'editolido',
+      "libraryTarget": options.target
+    },
+  };
+}
+
+module.exports = [
+    createConfig({"target": "var", "minimize": true}),
+    createConfig({"target": "var", "minimize": false}),
+    createConfig({"target": "umd", "minimize": true}),
+    createConfig({"target": "umd", "minimize": false}),
+];
