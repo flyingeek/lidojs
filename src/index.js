ecf8b1cd51ecd0752ebd91345d66d161d7358ea7
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-04-22 21:59:16 +0200

    forgot to export style, bumpversion 1.3.19

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ export const version = VERSION;
 export {GeoPoint, nm_to_rad, rad_to_nm} from "./modules/geopoint"
 export {AVENZAICONS, GOOGLEICONS, NAT_POSITION_ENTRY, NAT_POSITION_EXIT, PINS, PIN_BLUE, PIN_BROWN, PIN_GREEN, PIN_NONE, PIN_ORANGE, PIN_PINK, PIN_PURPLE, PIN_RED, PIN_YELLOW} from "./modules/kml_constants";
 export {Route, Track} from "./modules/route"
-export {avenzaIconTemplate, avenzaStyleTemplate, avenzaTemplate, iconTemplate, styleTemplate, template} from "./modules/kml_templates";
+export {googleEarthStyleTemplate, avenzaIconTemplate, avenzaStyleTemplate, avenzaTemplate, iconTemplate, styleTemplate, template} from "./modules/kml_templates";
 export {KMLGenerator} from "./modules/kml";
 export {Ofp} from "./modules/ofp";
 export {GEO_HASH_GRID_SIZE, GeoGridIndex} from "./modules/geoindex";
