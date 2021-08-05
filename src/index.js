de6a755fd967f8269ea982f8ce3c91a156070035
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-08-05 17:28:52 +0200

    added dm_normalizer to exported functions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /* globals VERSION */
 export const version = VERSION;
-export {GeoPoint, nm_to_rad, rad_to_nm, rad_to_deg} from "./modules/geopoint"
+export {GeoPoint, nm_to_rad, rad_to_nm, rad_to_deg, dm_normalizer} from "./modules/geopoint"
 export {AVENZAICONS, GOOGLEICONS, NAT_POSITION_ENTRY, NAT_POSITION_EXIT, PINS, PIN_BLUE, PIN_BROWN, PIN_GREEN, PIN_NONE, PIN_ORANGE, PIN_PINK, PIN_PURPLE, PIN_RED, PIN_YELLOW} from "./modules/kml_constants";
 export {Route, Track} from "./modules/route"
 export {googleEarthStyleTemplate, avenzaIconTemplate, avenzaStyleTemplate, avenzaTemplate, iconTemplate, styleTemplate, template} from "./modules/kml_templates";
