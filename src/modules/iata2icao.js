4d04503ec11d918ca68ef8e745cec9487a000fe0
Eric Delord <68456+flyingeek@users.noreply.github.com>
2021-12-08 18:54:56 +0100

    added airports timezones

--- src/modules/iata2icao.js
+++ src/modules/iata2icao.js
@@ -1,6 +1,7 @@
 import {GeoPoint} from './geopoint';
 const AIRPORTS = require('./airports.json');
 const IATADB = require('./iata2icao.json');
+const TZDB = require('./timezones.json');
 
 /** return icao code for a iata code or '????' */
 export function iata2icao(iata) {
@@ -12,6 +13,46 @@ export function iata2icao(iata) {
 export function icao2iata(icao) {
     const index = IATADB.indexOf(':' + icao);
     return (index >= 0) ? IATADB.substring(index - 3, index) : '???';
+}
+
+/** return iata timezone "Europe/Paris" or undefined*/
+export function iata2tz(iata) {
+    const index = IATADB.indexOf(iata + ':');
+    if (index >= 0) {
+        const tz = IATADB.substring(index + 8, index + 10);
+        return (tz === '00') ? undefined : TZDB[tz];
+    }
+    return undefined;
+}
+
+/** return timezone offset "-2" "+5.5" for iata at a date like "2021-08-12T14:25Z" or undefined */
+export function tzOffset(iata, isoString) {
+    const timeZone = iata2tz(iata);
+    if (!timeZone) return undefined;
+    let event = new Date(Date.parse(isoString));
+    // British English uses day/month/year order and 24-hour time without AM/PM
+    // eslint-disable-next-line init-declarations
+    let loc;
+    try {
+        loc = event.toLocaleString("en-GB", {timeZone});
+    } catch (e) {
+        return undefined;
+    }
+    const re = /(\d\d)\/(\d\d)\/(\d\d\d\d), (\d\d):(\d\d):\d\d/u
+    const match = re.exec(loc);
+    if (match !== null) {
+        const [, day, month, year, hour, minute] = match;
+        const baseIsoString = `${year}-${month}-${day}T${hour}:${minute}`;
+        const baseEvent = new Date(Date.parse(baseIsoString + "Z"));
+        const offset = (baseEvent - event)/3600000;
+        if (offset === 0) {
+            return "+0";
+        }
+        let res = (offset >= 0) ? '+' : '-';
+        res += offset.toFixed(1)
+        return (res.endsWith('.0')) ? res.slice(0, -2) : res;
+    }
+    return undefined
 }
 
 /** return a LatLng from a iata code */
--- test/iata.test.js
+++ test/iata.test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import {iata2GeoPoint, iata2icao, icao2iata} from '../src/modules/iata2icao.js';
+import {iata2GeoPoint, iata2icao, iata2tz, icao2iata, tzOffset} from '../src/modules/iata2icao.js';
 import {GeoPoint} from '../src/modules/geopoint';
 
 test('iata2icao', () => {
@@ -16,4 +16,14 @@ test('iata2LatLng', () => {
     expect(g).toBeInstanceOf(GeoPoint);
     expect(g.latlng.asDM).toBe('N4900.6E00232.9');
     expect(g.name).toBe('LFPG');
+});
+test('iata2tz', () => {
+    expect(iata2tz('???')).toBe(undefined);
+    expect(iata2tz('CDG')).toBe('Europe/Paris');
+});
+test('tzOffset', () => {
+    expect(tzOffset('???', '2021-08-01T14:00Z')).toBe(undefined);
+    expect(tzOffset('CDG', '2021-08-01T14:00Z')).toBe('+2');
+    expect(tzOffset('CDG', '2021-12-01T14:00Z')).toBe('+1');
+    expect(tzOffset('DEL', '2021-12-01T14:00Z')).toBe('+5.5');
 });
